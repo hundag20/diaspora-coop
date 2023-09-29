c24f36111638758b03a261c276bb0e3224858f55
hundag20 <hundaguluma@gmail.com>
2023-09-29 09:42:15 +0300

    removed junk files and created skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactNode } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
-function App() {
+interface BodyeRouteProps { // TODO: move to types
+  children: ReactNode;
+}
+
+const BodyRoute = ({ children }: BodyeRouteProps) =>
+  <div className='app'>
+    <header>
+      <Header />
+    </header>
+    <body>
+      {children}
+    </body>
+    <footer>
+      <Footer />
+    </footer>
+  </div>
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={
+          <BodyRoute>
+            <Home />
+          </BodyRoute>
+        } />
+      </Routes>
     </div>
   );
 }
-
 export default App;
