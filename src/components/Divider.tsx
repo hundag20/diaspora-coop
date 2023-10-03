8524c84e7bdf9cd04b647b71888b3cb1009f07b2
hundag20 <hundaguluma@gmail.com>
2023-10-03 11:40:03 +0300

    done money-transfer page

---
+++ src/components/Divider.tsx
@@ -0,0 +1,13 @@
+import '../styles/divider.scss'
+
+export interface IDividerProps {
+}
+
+export function Divider(props: IDividerProps) {
+    return (
+        <div className='divider'>
+            <div className="main">
+            </div>
+        </div>
+    );
+}
