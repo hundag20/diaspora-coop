0cf359595ac41c2249c63a3b8cac5fd4ca40fbef
Hundaol NK <hundaolnk@gmail.com>
2023-10-03 15:19:08 +0300

    working on navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,22 @@ import * as React from "react";
 import cooplogo from "../assets/img/cooplogo.png";
 import "../styles/headers.scss";
 import MainButton from "./Buttons/mainButton";
+import { useState } from "react";
 export interface IHeaderProps {}
 
+const myNumber: number | null = null;
+
 export function Header(props: IHeaderProps) {
+  const [openSubMenu, setOpenSubMenu] = useState<number | null>(null);
+
+  interface MyHandleFunctionState {
+    count: number;
+    message: string;
+  }
+  const handleSubMenuToggle = (index: number | null) => {
+    setOpenSubMenu(openSubMenu === index ? null : index);
+  };
+
   const menuItems = [
     {
       label: "Home",
@@ -105,15 +118,34 @@ export function Header(props: IHeaderProps) {
         <div className="right">
           <ul>
             {menuItems.map((menuItem, index) => (
-              <li key={index}>
+              <li
+                key={index}
+                className={`menu-item ${menuItem.subMenu ? "has-submenu" : ""}`}
+                onMouseEnter={() => handleSubMenuToggle(index)}
+                onMouseLeave={() => handleSubMenuToggle(null)}
+              >
                 {menuItem.subMenu ? (
                   <>
                     {menuItem.label}
-                    {/* <ul>
-                      {menuItem.subMenu.map((subItems, subIndex) => (
-                        <li key={subIndex}>{subItems}</li>
-                      ))}
-                    </ul> */}
+                    {menuItem.subMenu && (
+                      <i aria-hidden="true" className="far fa-address-card"></i>
+                    )}
+                    {menuItem.subMenu && openSubMenu === index && (
+                      <div className="submenu">
+                        {menuItem.subMenu.map((subItem, subIndex) => (
+                          <div key={subIndex} className="submenu-item">
+                            <i
+                              aria-hidden="true"
+                              className="icon icon-save-money"
+                            ></i>
+                            <span className="submenu-label">
+                              label
+                              {/* {subItem.label} */}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
+                    )}
                   </>
                 ) : (
                   menuItem.label
