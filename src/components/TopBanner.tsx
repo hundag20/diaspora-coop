ed150caa394d46af95998707dc1d720add7253b1
hundag20 <hundaguluma@gmail.com>
2023-10-11 10:23:55 +0300

    fixed responsiveness issues

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,28 +1,26 @@
-import '../styles/topBanner.scss'
-import famousMonumnets from '../assets/img/famous-monuments-world_117023-91-1.jpg'
-import waveSvg from '../assets/img/wave.svg'
+import '../styles/topBanner.scss';
+import famousMonumnets from '../assets/img/famous-monuments-world_117023-91-1.jpg';
+import waveSvg from '../assets/img/wave.svg';
 export interface ITopBannerProps {
-    containerClass: string
-    overlayClass: string
-    contentClass: string
-    footerClass: string
-    contentHeader: string
-    contentParagraph: string
+  containerClass: string;
+  overlayClass: string;
+  contentClass: string;
+  footerClass: string;
+  contentHeader: string;
+  contentParagraph: string;
 }
 
 export function TopBanner(props: ITopBannerProps) {
-    return (
-        <div className={props.containerClass}>
-            <div className={props.overlayClass}>
-            </div>
-            <div className={props.contentClass}>
-                <h1>{props.contentHeader}</h1>
-                <p>{props.contentParagraph}
-                </p>
-            </div>
-            <div className={props.footerClass}>
-                <img className="svg" src={waveSvg}></img>
-            </div>
-        </div>
-    );
+  return (
+    <div className={props.containerClass}>
+      <div className={props.overlayClass}></div>
+      <div className={props.contentClass}>
+        <h1>{props.contentHeader}</h1>
+        <p id='banner-subheader'>{props.contentParagraph}</p>
+      </div>
+      <div className={props.footerClass}>
+        <img className='svg' src={waveSvg}></img>
+      </div>
+    </div>
+  );
 }
