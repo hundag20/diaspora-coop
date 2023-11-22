6077da8575e36888711a00fd7f52aec5cb90cbf6
Elshaday Zewdie <elshadayzewude@gmail.com>
2023-11-22 16:00:04 +0300

    swiper import removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,15 +35,6 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import { useTheme } from "@mui/material/styles";
 
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 export interface IHomeProps {}
 
