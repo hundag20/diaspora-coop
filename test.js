ad02b0bdbc4184fad1e69142efa104d29df2e04d
hundag20 <hundaguluma@gmail.com>
2024-02-22 16:04:59 +0300

    change PUT methods to POST and updated endpoints

--- test.js
+++ test.js
@@ -11,6 +11,6 @@ const { default: axios } = require("axios");
 //   "method": "PUT"
 // }).then(data => console.log('data', data)).catch(err => console.log('err',err));
 
-axios.put("https://10.191.1.90:8881/api/v1/accounts/30", 
+axios.post("https://10.191.1.90:8881/api/v1/accounts/update/30", 
 JSON.parse("{\"fullName\":\"HundaGulumaSobokssa\",\"surname\":\"HundaGulumaSobokssa\",\"motherName\":\"HundaGulumaSobokssa\",\"sex\":\"MALE\",\"email\":\"hundaguluma@gmail.com\",\"phone\":\"+251930586155\"}"),
 ).then(data => console.log('d', data)).catch(err => console.log('err', err))
